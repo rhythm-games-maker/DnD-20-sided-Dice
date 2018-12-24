60959b4046ead9f61b21f29ad9407af0887dcd1f
r3tr0-Dev <45609267+r3tr0-Dev@users.noreply.github.com>
2018-12-23 19:58:02 -0500

    Add files via upload
    shortened it up and made the class a bit more classy ;)

--- dice.py
+++ dice.py
@@ -1,64 +1,43 @@
 from graphics import *
 from random import randint
 
-wn = GraphWin('DnD Dice Generator', 400,400)
-
-class MainDice:
-    def __init__(self):
-        self.position = Point(200,130)
-        self.image = Image(self.position, 'dice.png')
-        return
-
-    def create(self, wn):
-        self.image.draw(wn)
-        return
-
-
-class luckyNum():
-    def __init__(self, text):
-        self.text = text
-        self.position = Point(197, 122)
-        self.numberTxt = Text(self.position, self.text)
+class Dice:
+    def __init__(self, x, y):
+        self.position = Point(x,y)
+        self.image = 'dice.png'
+        self.number = ''
+        self.dice = Image(self.position, self.image)
+        self.luckyNumber = Text(self.position, self.number)
 
     def create(self, wn):
-        self.numberTxt.setSize(20)
-        self.numberTxt.setFill(color_rgb(255,0,0))
-        self.numberTxt.draw(wn)
+        self.luckyNumber.setSize(20)
+        self.luckyNumber.setFill(color_rgb(255, 0, 0))
+        self.luckyNumber.draw(wn)
+        self.dice.draw(wn)
         return
 
-    def destroy(self):
-        self.numberTxt.undraw()
-        return  
+    def roll(self):
+        randNum = randint(1,20)
+        self.number = str(randNum)
+        return
 
-def randomNum():
-    rand = randint(1,20)
-    num = str(rand)
-    return num
-
+    def set(self):
+        self.luckyNumber.setText(self.number)
+        return
 
 def main():
-    global time    
-    mydice = MainDice()
-    mydice.create(wn)
+    width = 400
+    height = 400
+    name = '20-Sided Dice'
 
-    clickTxt = Text(Point(200,300), "Click to roll dice.")
-    clickTxt.setSize(22)
-    clickTxt.draw(wn)
-    
+    wn = GraphWin(name, width,height)
+
+    myDice = Dice(200,150)
+    myDice.create(wn)
+
     while True:
         mouse = wn.getMouse()
-        num = randomNum()
-        
         if mouse:
-            myNum = luckyNum(num)
-            myNum.create(wn)
-            
-            unclickTxt = Text(Point(200,350), "Click again to erase!")
-            unclickTxt.setSize(18)
-            unclickTxt.draw(wn)
-            
-            mouse = wn.getMouse()
-            if mouse:
-               myNum.destroy()
-               unclickTxt.undraw()
+            myDice.roll()
+            myDice.set()
 main()
