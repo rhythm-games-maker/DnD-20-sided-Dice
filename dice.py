4a0ac22f83dad5a096a1f3e7cb4d61a280674d96
r3tr0-Dev <45609267+r3tr0-Dev@users.noreply.github.com>
2018-12-24 08:07:22 -0500

    Add files via upload
    added roll button

--- dice.py
+++ dice.py
@@ -4,7 +4,7 @@ from random import randint
 class Dice:
     def __init__(self, x = 200, y = 200):
         self.position = Point(x,y)
-        self.numberPosition = Point(197, 193)
+        self.numberPosition = Point(x-3, x-7)
         self.image = 'dice.png'
         self.number = ''
         self.dice = Image(self.position, self.image)
@@ -25,20 +25,50 @@ class Dice:
     def set(self):
         self.luckyNumber.setText(self.number)
         return
+    
+class Button:
+    def __init__(self):
+        self.x = 200
+        self.y = 325
+        
+        self.image = 'rollBtn.png'
+        self.button = Image(Point(self.x,self.y), self.image)
+        self.text = Text(Point(self.x,self.y), 'ROLL')
+        return
 
+    def create(self, wn):
+        self.button.draw(wn)
+        self.text.setFill(color_rgb(255,255,255))
+        self.text.draw(wn)
+        return
+
+    def collide(self,wn):
+        mouse = wn.getMouse()
+        mx = mouse.x
+        my = mouse.y
+        
+        dx = abs(self.x - mx)
+        dy = abs(self.y - my)
+
+        if dx < 40 and dy < 30:
+            return True
+        
 def main():
     width = 400
     height = 400
     name = '20-Sided Dice'
 
     wn = GraphWin(name, width,height)
-
+    
     myDice = Dice()
     myDice.create(wn)
 
+    rollBtn = Button()
+    rollBtn.create(wn)
+    
     while True:
-        mouse = wn.getMouse()
-        if mouse:
+        torf = rollBtn.collide(wn)
+        if torf == True:
             myDice.roll()
-            myDice.set()
+            myDice.set()        
 main()
